700ca1c3c8f77356028867b8bb9d3dd28c12e9f0
hfyu <hfyu@ctrip.com>
2014-12-31 15:00:31 +0800

    routes list

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ var routerController = require("./routes/controller").controller;
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('jade', require('jade').__express);
+app.engine('ejs', require('ejs').__express);
+//app.engine('jade', require('jade').__express);
 //@see https://www.npmjs.com/package/morgan 日志格式参考这里format.
 app.use(morgan('HTTP/:http-version :method :status :url :response-time ms :date :user-agent :referrer'));
 app.use(express.static(__dirname + '/public'));
 
 app.get(/^\/(index|home)?$/, routerController.index);
 app.get(/^\/count$/, routerController.count);
+//app.get(/^\/add/, routerController.add);
 app.get(/^\/\w+(\/)?(\w+)?$/, routerController.list);
 
 var server = app.listen(app.get('port'), function() {
--- routes/list.js
+++ routes/list.js
@@ -1,3 +1,33 @@
-exports.index = function(req, res){
-  res.send("this is list");
+var Mock = require("mockjs");
+
+exports.index = function(req, res) {
+	var template = {
+		"errno": 0,
+		"msg": "",
+		"data|6": [{
+			"id|+1": 1,
+			"img": "@img(24x24)",
+			"name|1": ["html", "css", "框架/类库", "开发/调试工具", "性能/测试"],
+			"enname": "@title",
+			"parentid": 0,
+			"rank|1-6": 1,
+			"quality|1-100": 1,
+			"add_date": "@date",
+			"last_modified": "@date",
+			"sublist|5-10": [{
+				"id|+1": 7,
+				"img": "@img(24x24)",
+				"name|1": ["html", "css", "框架", "类库", "开发", "调试工具", "性能", "测试"],
+				"enname": "@title",
+				"parentid|1-6": 1,
+				"rank|1-6": 1,
+				"quality|1-100": 1,
+				"add_date": "@date",
+				"last_modified": "@date",
+			}]
+		}]
+	};
+
+	var jsonData = Mock.mock(template);
+	res.send(JSON.stringify(jsonData, null, 4));
 };
